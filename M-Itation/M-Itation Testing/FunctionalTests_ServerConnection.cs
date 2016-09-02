32d16a9c89312ad042d27d08cf89a950cfafc5f8
Brian Clark <blclark4@gmail.com>
2016-09-02 18:03:28 -0500

    Added library project and functional test

---
+++ M-Itation/M-Itation Testing/FunctionalTests_ServerConnection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using M_Itation;
+using MFilesAPI;
+
+namespace M_Itation_Testing
+{
+    [TestClass]
+    public class FunctionalTests_ServerConnection
+    {
+        [TestMethod]
+        public void GetServerInfo()
+        {
+            // Get the library and see if the server is where we expect it to be.
+            var library = new MItationLibrary();
+            Assert.IsNotNull(library);
+            library.Connect();
+
+            // Verify that you're connected to the server
+            Assert.IsTrue(library.IsConnectedtoServer, "After connecting... Library not connected.");
+
+            // Get a list of vaults we can connect to.
+            var vaults = library.GetVaults();
+            Assert.IsNotNull(vaults);
+            Assert.AreNotEqual(0, vaults.Count());
+            // Connect to the first vault in the list
+
+
+
+            
+  
+
+
+
+
+
+            // Once you've covered this case, remove this item.
+            Assert.Fail("Finish the GetServerInfo functional test");
+        }   
+    }
+}
---
+++ M-Itation/M-Itation/MItationLibrary.cs
@@ -0,0 +1,48 @@
+﻿using MFilesAPI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M_Itation
+{
+    public class MItationLibrary
+    {
+        public bool IsConnectedtoServer
+        {
+            get
+            {
+                bool isConnected = false;
+                if (Server != null)
+                {
+                    try
+                    {
+                        Server.TestConnectionToServerEx();
+                        isConnected = true;
+                    }
+                    catch
+                    {
+                        // TODO:  Make a custom exception.
+                        throw;
+                    }
+                }
+                return isConnected;
+            }
+
+        }
+
+        internal MFilesServerApplication Server { get; private set; }
+
+        public void Connect()
+        {
+            Server = new MFilesServerApplication();
+            Server.Connect(AuthType: MFAuthType.MFAuthTypeLoggedOnWindowsUser, ProtocolSequence: "ncacn_ip_tcp", LocalComputerName: Environment.MachineName);
+        }
+
+        public string[] GetVaults()
+        {
+            return Server.GetVaults().OfType<VaultOnServer>().Select(vos => vos.Name).ToArray(); 
+        }
+    }
+}
